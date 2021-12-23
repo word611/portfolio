aefc416745f308da19f97318e420dbd32aebb6c3
word611 <dotjszbxm@naver.com>
2021-12-23 20:12:02 +0900

    portfolio update

--- Chrome/js/clock.js
+++ Chrome/js/clock.js
@@ -10,3 +10,20 @@ function getClock(){
 
 getClock()
 setInterval(getClock, 1000);
+
+/*
+const clockTitle = document.querySelector(".js-clock");
+
+function getday() {
+  const dday = new Date("2021-12-25:00:00:00+0900");
+  const tday = new Date();
+  const gap = dday - tday;
+  const days = Math.floor(gap / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((gap / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor(((gap / 1000) * 60) % 60);
+  const seconds = Math.floor((gap / 1000) % 60);
+
+  clockTitle.innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
+getday();
+setInterval(getday, 1000);*/
